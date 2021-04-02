81a040fc919a1fc9af00d82792651a3908f2468c
james_ryans <james.ryans2012@gmail.com>
2021-04-02 12:09:26 +0700

    WIP: notifee background event

--- index.js
+++ index.js
@@ -5,14 +5,29 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import notifee, { AndroidImportance } from '@notifee/react-native';
+import notifee, { AndroidImportance, EventType } from '@notifee/react-native';
+import { Linking } from 'react-native';
 
 notifee.createChannel({
-  id: 'default-channel-id',
+  id: 'download-channel-id',
   name: 'Downloads',
   vibration: true,
   importance: AndroidImportance.HIGH,
   badge: true,
 });
+
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  const { notification, pressAction } = detail;
+
+  if (type === EventType.PRESS) {
+    const initialNotification = notifee.getInitialNotification();
+    console.log('notification caused app to open', initialNotification.notification);
+    console.log('press action used to open the app', initialNotification.pressAction);
+  }
+
+  if (type === EventType.ACTION_PRESS && pressAction.id === 'gallery') {
+    Linking.openURL('content://media/internal/images/media');
+  }
+})
 
 AppRegistry.registerComponent(appName, () => App);
--- src/services/NotificationController.js
+++ src/services/NotificationController.js
@@ -1,14 +1,21 @@
 import notifee, { AndroidBadgeIconType, AndroidColor, AndroidImportance } from '@notifee/react-native';
 
-const FinishedDownloadNotification = () => {
-  notifee.displayNotification({
+const FinishedDownloadNotification = async () => {
+  await notifee.displayNotification({
     title: 'Download Completed',
     body: 'Your download is completed, press the button below to open gallery',
     android: {
-      channelId: 'default-channel-id',
+      channelId: 'download-channel-id',
       importance: AndroidImportance.HIGH,
-      badgeIconType: AndroidBadgeIconType.LARGE,
-    }
+      actions: [
+        {
+          title: 'Gallery',
+          pressAction: {
+            id: 'gallery',
+          },
+        },
+      ],
+    },
   });
 };
 
